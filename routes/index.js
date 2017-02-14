f0abda1f207d1c6ad6d54183ada6f7758bef0ae8
zideajang <zidea2015@163.com>
2017-02-14 14:51:19 +0800

    添加页面

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 主页面 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '主页' });
+}); 
+
+/* 注册页面. */
+router.get('/reg', function(req, res, next) {
+  res.render('reg', { title: '注册' });
 });
+
+/* 注册页面. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: '登录' });
+});
+
+
 
 module.exports = router;
